53e9e16266932bade1237cd09be6f16da51827bd
Александра Семерня <qwr1235@mail.ru>
2021-12-02 18:50:20 +0700

    Models

--- mainApp/views.py
+++ mainApp/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'home1.html')
+
+def register(request):
+
+    if request.method == 'POST':
+        data = request.POST.copy()
+        errors = form.get_validation_errors(data)
+        if not errors:
+            new_user = form.save(data)
+            return HttpResponseRedirect("/books/")
+    else:
+        data, errors = {}, {}
+
+    return render(request,"registration/register.html")
